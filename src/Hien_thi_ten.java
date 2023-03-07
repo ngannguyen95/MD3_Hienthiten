6adce74d2bdd45fa4fc6539f35c48820e9cf6525
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-03-07 10:32:22 +0700

    hello

---
+++ src/Hien_thi_ten.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Hien_thi_ten {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhập vào tên: ");
+        String name=scanner.nextLine();
+        System.out.println("Xin chào: "+name);
+    }
+}
